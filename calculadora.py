aa0e02d09d437e6c550fc3afbebed5a1eb3b86c6
Guss319 <guss372@icloud.com>
2023-09-23 09:18:00 -0300

    se cambian al español los nombres de las funciones suma y resta

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,18 @@
-def add(x, y):
+def suma(x, y):
     return x + y
 
-def subtract(x, y):
+
+def resta(x, y):
     return x - y
+
 
 def multiply(x, y):
     return x * y
 
+
 def divide(x, y):
     return x / y
+
 
 print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit")
 
@@ -20,10 +24,10 @@ while True:
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, "=", suma(num1, num2))
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(num1, "-", num2, "=", resta(num1, num2))
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
@@ -36,5 +40,3 @@ while True:
         break
     else:
         print("Entrada invalida")
-
-
